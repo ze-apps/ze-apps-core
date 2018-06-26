321647bc8ef261b4c9dbc04679622271d8566cea
Nicolas Ramel <nramel@r-numerique.fr>
2018-06-27 00:15:22 +0200

    seed dev

---
+++ system/Database/Seed/2018_03_04_15_00_22_SeedDefaultLoadingModules.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Zeapps\Models\User;
+
+class SeedDefaultLoadingModules
+{
+    public function run()
+    {
+        Capsule::table('zeapps_modules')->insert([
+            'module_id' => "com_zeapps_contact",
+            'label' => "com_zeapps_contact",
+            'active' => "1",
+            'version' => "1.0.0",
+            'last_sql' => "0",
+            'dependencies' => "",
+            'missing_dependencies' => "",
+            'created_at'=>'2018-01-01',
+            'updated_at'=>'2018-01-01',
+        ]);
+
+
+        Capsule::table('zeapps_modules')->insert([
+            'module_id' => "com_zeapps_crm",
+            'label' => "com_zeapps_crm",
+            'active' => "1",
+            'version' => "1.0.0",
+            'last_sql' => "0",
+            'dependencies' => "",
+            'missing_dependencies' => "",
+            'created_at'=>'2018-01-01',
+            'updated_at'=>'2018-01-01',
+        ]);
+    }
+}
--- system/Database/Seed/2018_03_04_15_00_22_SeedZeappsUsers.php
+++ system/Database/Seed/2018_03_04_15_00_22_SeedZeappsUsers.php
@@ -8,11 +8,42 @@ class SeedZeappsUsers
     public function run()
     {
         Capsule::table('zeapps_users')->insert([
+            'id'=>1,
             'firstname' => "John",
             'lastname' => "Doe",
             'email' => 'john.doe@gmail.com',
             'lang' => 'en',
             'password' => hash(User::getTypeHash(), 'password'),
+            'created_at'=>'2018-01-01',
+            'updated_at'=>'2018-01-01',
+        ]);
+
+
+
+        // creation d'un groupe
+        Capsule::table('zeapps_groups')->insert([
+            'id'=>1,
+            'label' => "Testeur",
+            'rights' => json_encode(array(
+                "com_zeapps_project_write"=>1,
+                "com_zeapps_project_read"=>1,
+                "com_zeapps_project_management"=>1,
+                "com_zeapps_project_financial"=>1,
+                "com_zeapps_contact_read"=>1,
+                "com_zeapps_contact_write"=>1
+            )),
+            'created_at'=>'2018-01-01',
+            'updated_at'=>'2018-01-01',
+        ]);
+
+
+        // Adhésion au groupe
+        Capsule::table('zeapps_user_groups')->insert([
+            'id'=>1,
+            'id_user'=>1,
+            'id_group'=>1,
+            'created_at'=>'2018-01-01',
+            'updated_at'=>'2018-01-01',
         ]);
     }
 }
