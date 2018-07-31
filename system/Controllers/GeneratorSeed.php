260fb2d8a0692dc264475d34998d390f0c01f287
Nicolas Ramel <nramel@r-numerique.fr>
2018-07-31 19:15:07 +0200

    seed generator for dev

---
+++ system/Controllers/GeneratorSeed.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Zeapps\Controllers;
+
+use Zeapps\Core\Controller;
+use Zeapps\Core\Request;
+use Zeapps\Core\Session;
+
+use App\com_zeapps_contact\Models\AccountingNumbers;
+
+use App\com_zeapps_crm\Models\ProductCategories;
+use App\com_zeapps_crm\Models\ProductProducts;
+use App\com_zeapps_crm\Models\ProductLines;
+use App\com_zeapps_crm\Models\ProductStocks;
+
+use App\com_zeapps_crm\Models\Quotes;
+use App\com_zeapps_crm\Models\QuoteLines;
+use App\com_zeapps_crm\Models\QuoteLineDetails;
+use App\com_zeapps_crm\Models\QuoteActivities;
+
+use App\com_zeapps_crm\Models\Orders;
+use App\com_zeapps_crm\Models\OrderLines;
+use App\com_zeapps_crm\Models\OrderLineDetails;
+use App\com_zeapps_crm\Models\OrderActivities;
+
+use App\com_zeapps_crm\Models\Invoices;
+use App\com_zeapps_crm\Models\InvoiceLines;
+use App\com_zeapps_crm\Models\InvoiceLineDetails;
+use App\com_zeapps_crm\Models\InvoiceActivities;
+
+use App\com_zeapps_crm\Models\Deliveries;
+use App\com_zeapps_crm\Models\DeliveryLines;
+use App\com_zeapps_crm\Models\DeliveryLineDetails;
+use App\com_zeapps_crm\Models\DeliveryActivities;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+
+class GeneratorSeed extends Controller
+{
+    public function generate()
+    {
+        // AccountingNumbers
+        $this->save("com_zeapps_contact", "AccountingNumbers.json", json_encode(AccountingNumbers::get(), JSON_PRETTY_PRINT));
+
+
+
+        // ProductCategories
+        $this->save("com_zeapps_crm", "ProductCategories.json", json_encode(ProductCategories::get(), JSON_PRETTY_PRINT));
+
+        // ProductProducts
+        $this->save("com_zeapps_crm", "ProductProducts.json", json_encode(ProductProducts::get(), JSON_PRETTY_PRINT));
+
+        // ProductLines
+        $this->save("com_zeapps_crm", "ProductLines.json", json_encode(ProductLines::get(), JSON_PRETTY_PRINT));
+
+        // ProductStocks
+        $this->save("com_zeapps_crm", "ProductStocks.json", json_encode(ProductStocks::get(), JSON_PRETTY_PRINT));
+
+
+
+        // Quotes
+        $this->save("com_zeapps_crm", "Quotes.json", json_encode(Quotes::get(), JSON_PRETTY_PRINT));
+
+        // QuoteLines
+        $this->save("com_zeapps_crm", "QuoteLines.json", json_encode(QuoteLines::get(), JSON_PRETTY_PRINT));
+
+        // QuoteLineDetails
+        $this->save("com_zeapps_crm", "QuoteLineDetails.json", json_encode(QuoteLineDetails::get(), JSON_PRETTY_PRINT));
+
+        // QuoteActivities
+        $this->save("com_zeapps_crm", "QuoteActivities.json", json_encode(QuoteActivities::get(), JSON_PRETTY_PRINT));
+
+
+
+
+        // Orders
+        $this->save("com_zeapps_crm", "Orders.json", json_encode(Orders::get(), JSON_PRETTY_PRINT));
+
+        // OrderLines
+        $this->save("com_zeapps_crm", "OrderLines.json", json_encode(OrderLines::get(), JSON_PRETTY_PRINT));
+
+        // OrderLineDetails
+        $this->save("com_zeapps_crm", "OrderLineDetails.json", json_encode(OrderLineDetails::get(), JSON_PRETTY_PRINT));
+
+        // OrderActivities
+        $this->save("com_zeapps_crm", "OrderActivities.json", json_encode(OrderActivities::get(), JSON_PRETTY_PRINT));
+
+
+
+
+        // Invoices
+        $this->save("com_zeapps_crm", "Invoices.json", json_encode(Invoices::get(), JSON_PRETTY_PRINT));
+
+        // InvoiceLines
+        $this->save("com_zeapps_crm", "InvoiceLines.json", json_encode(InvoiceLines::get(), JSON_PRETTY_PRINT));
+
+        // InvoiceLineDetails
+        $this->save("com_zeapps_crm", "InvoiceLineDetails.json", json_encode(InvoiceLineDetails::get(), JSON_PRETTY_PRINT));
+
+        // InvoiceActivities
+        $this->save("com_zeapps_crm", "InvoiceActivities.json", json_encode(InvoiceActivities::get(), JSON_PRETTY_PRINT));
+
+
+
+
+
+        // Deliveries
+        $this->save("com_zeapps_crm", "Deliveries.json", json_encode(Deliveries::get(), JSON_PRETTY_PRINT));
+
+        // DeliveryLines
+        $this->save("com_zeapps_crm", "DeliveryLines.json", json_encode(DeliveryLines::get(), JSON_PRETTY_PRINT));
+
+        // DeliveryLineDetails
+        $this->save("com_zeapps_crm", "DeliveryLineDetails.json", json_encode(DeliveryLineDetails::get(), JSON_PRETTY_PRINT));
+
+        // DeliveryActivities
+        $this->save("com_zeapps_crm", "DeliveryActivities.json", json_encode(DeliveryActivities::get(), JSON_PRETTY_PRINT));
+
+    }
+
+    private function save($module, $seedname, $content)
+    {
+
+        $file = MODULEPATH . $module . "/Database/Seed/" . $seedname;
+
+        if (file_exists($file)) {
+            unlink($file);
+        }
+        $handle = fopen($file, 'w') or die('Cannot open file:  ' . $file);
+        fwrite($handle, $content);
+        fclose($handle);
+    }
+}
--- system/route/test.php
+++ system/route/test.php
@@ -5,3 +5,7 @@ use Zeapps\Core\Routeur ;
 // TODO : c'est à supprimer, cela servait pour les tests
 Routeur::get('/test/{idProduit}-{nomProduit}', 'Zeapps\\Controllers\\Auth@index')->name('produit');
 Routeur::get('/sales/{idCategorie}/{idProduit}-{nomCategorie}', 'Zeapps\\Controllers\\Auth@test')->name('compte-client');
+
+
+// pour générer les seeds pendant la phase de dev
+Routeur::get('/generete-seed', 'Zeapps\\Controllers\\GeneratorSeed@generate');
