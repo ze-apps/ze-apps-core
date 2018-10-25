615ce705e268631e04930c00ae4dba374bb71d14
Aziz Bennane <aziz.bennane@preview-communication.fr>
2018-10-25 16:54:37 +0200

    *** save 25/10/18 ***

--- system/Database/Seed/2018_03_04_15_00_22_SeedDefaultLoadingModules.php
+++ system/Database/Seed/2018_03_04_15_00_22_SeedDefaultLoadingModules.php
@@ -33,5 +33,17 @@ class SeedDefaultLoadingModules
             'created_at'=>'2018-01-01',
             'updated_at'=>'2018-01-01',
         ]);
+
+        Capsule::table('zeapps_modules')->insert([
+            'module_id' => "com_zeapps_opportunity",
+            'label' => "com_zeapps_opportunity",
+            'active' => "1",
+            'version' => "1.0.0",
+            'last_sql' => "0",
+            'dependencies' => "",
+            'missing_dependencies' => "",
+            'created_at'=>'2018-01-01',
+            'updated_at'=>'2018-01-01',
+        ]);
     }
 }
