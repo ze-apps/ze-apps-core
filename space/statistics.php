cfd0405860e1a915262dcb69b6867b8cf6070bce
Aziz Bennane <aziz.bennane@preview-communication.fr>
2018-11-28 08:59:01 +0100

    space statistique

---
+++ space/statistics.php
@@ -0,0 +1,11 @@
+<?php
+$spaceData = array () ;
+$spaceData["id"] = "com_zeapps_statistics" ;
+$spaceData["fa-icon"] = "signal" ;
+$spaceData["name"] = "Statistiques" ;
+$spaceData["color"] = "#ffffff" ;
+$spaceData["background-color"] = "#760692" ;
+$spaceData["menu-header"] = array() ;
+$spaceData["menu-header"]["col"] = 2 ;
+$spaceData["menu-header"]["order"] = 1 ;
+$space[] = $spaceData ;
