243ba372ddcbec473f5f70a445f0b41d88f68dce
Nicolas Ramel <nramel@r-numerique.fr>
2018-12-09 13:17:09 +0100

    update modal Search to add param on load modal

--- system/angularjs/_directives/zeModalsearch.js
+++ system/angularjs/_directives/zeModalsearch.js
@@ -1,74 +1,80 @@
-app.directive("zeModalsearch", ["zeapps_modal", function(zeapps_modal){
+app.directive("zeModalsearch", ["zeapps_modal", function (zeapps_modal) {
 
-	return {
-		restrict: "A",
-		scope: {
+    return {
+        restrict: "A",
+        scope: {
             zeModalsearch: '&',
-			http: '=',
+            http: '=',
             fields: '=',
-			model: '=',
-			filters: '=',
-			templateNew: '=',
-			title: '@'
-		},
-		template: 	"<div class=\"input-group\">" +
-						"<input type=\"text\" ng-model=\"model\" class=\"form-control\" disabled>" +
-						"<span class=\"input-group-btn\">" +
-							"<button class=\"btn btn-default\" type=\"button\" ng-click=\"clear()\"" +
-							"ng-show=\"model != '' && model != undefined\">x" +
-							"</button>" +
-							"<button class=\"btn btn-default\" type=\"button\" ng-click='openModal()'>...</button>" +
-						"</span>" +
-					"</div>",
-		link: function($scope){
-		    $scope.openModal = openModal;
-		    $scope.clear = clear;
+            model: '=',
+            filters: '=',
+            templateNew: '=',
+            title: '@',
+            onloadmodal: '=',
+            onloadmodalparam: '='
+        },
+        template: "<div class=\"input-group\">" +
+            "<input type=\"text\" ng-model=\"model\" class=\"form-control\" disabled>" +
+            "<span class=\"input-group-btn\">" +
+            "<button class=\"btn btn-default\" type=\"button\" ng-click=\"clear()\"" +
+            "ng-show=\"model != '' && model != undefined\">x" +
+            "</button>" +
+            "<button class=\"btn btn-default\" type=\"button\" ng-click='openModal()'>...</button>" +
+            "</span>" +
+            "</div>",
+        link: function ($scope) {
+            $scope.openModal = openModal;
+            $scope.clear = clear;
 
-		    function clear(){
-                if($scope.zeModalsearch() instanceof Function) {
+            function clear() {
+                if ($scope.zeModalsearch() instanceof Function) {
                     $scope.zeModalsearch()(false);
-                }
-                else {
+                } else {
                     $scope.model = undefined;
                 }
-			}
+            }
 
-		    function openModal(){
-		    	var options = {
-		    		http: $scope.http,
-					fields: $scope.fields,
+            function openModal() {
+                var options = {
+                    http: $scope.http,
+                    fields: $scope.fields,
                     title: $scope.title,
-					filters: $scope.filters,
-					template: $scope.templateNew
-				};
+                    filters: $scope.filters,
+                    template: $scope.templateNew
+                };
 
-                zeapps_modal.loadModule("com_zeapps_core", "search_modal", options, function(objReturn) {
-					if(objReturn.id !== undefined) {
-						if($scope.zeModalsearch() instanceof Function) {
-							$scope.zeModalsearch()(objReturn);
-						}
-						else {
-							$scope.model = objReturn[$scope.zeModalsearch()];
-						}
-					}
-					else{
-						var formatted_data = angular.toJson(objReturn);
-						$scope.http.save(formatted_data).then(function(response){
-							if(response.data && response.data != "false"){
-								objReturn.id = response.data;
+                if ($scope.onloadmodal) {
+                    if ($scope.onloadmodalparam) {
+                        $scope.onloadmodal($scope.onloadmodalparam);
+                    } else {
+                        $scope.onloadmodal();
+                    }
+                }
 
-								if($scope.zeModalsearch() instanceof Function) {
-									$scope.zeModalsearch()(objReturn);
-								}
-								else {
-									$scope.model = objReturn[$scope.zeModalsearch()];
-								}
-							}
-						})
-					}
+                zeapps_modal.loadModule("com_zeapps_core", "search_modal", options, function (objReturn) {
+                    if (objReturn.id !== undefined) {
+                        if ($scope.zeModalsearch() instanceof Function) {
+                            $scope.zeModalsearch()(objReturn);
+                        } else {
+                            $scope.model = objReturn[$scope.zeModalsearch()];
+                        }
+                    } else {
+                        var formatted_data = angular.toJson(objReturn);
+                        $scope.http.save(formatted_data).then(function (response) {
+                            if (response.data && response.data != "false") {
+                                objReturn.id = response.data;
+
+                                if ($scope.zeModalsearch() instanceof Function) {
+                                    $scope.zeModalsearch()(objReturn);
+                                } else {
+                                    $scope.model = objReturn[$scope.zeModalsearch()];
+                                }
+                            }
+                        })
+                    }
                 });
-			}
+            }
         }
-	};
+    };
 
 }]);
