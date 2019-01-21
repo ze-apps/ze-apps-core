9780f1427a5ec66ea2f943ad413b8532d22cfee3
Aziz Bennane <aziz.bennane@preview-communication.fr>
2019-01-21 09:33:53 +0100

    cron check execution time

--- system/Controllers/Cron.php
+++ system/Controllers/Cron.php
@@ -3,9 +3,6 @@
 namespace Zeapps\Controllers;
 
 use Zeapps\Core\Controller;
-use Zeapps\Core\Request;
-use Zeapps\Core\Session;
-
 use Zeapps\Core\Event;
 
 
@@ -15,12 +12,40 @@ class Cron extends Controller
     {
         $listCron = Event::getCron();
 
-        // TODO : controller si la tâche doit être éxécutée
-
         foreach ($listCron as $cron) {
-            list($controller, $method) = explode("@", $cron->command);
-            $ctrl = new $controller;
-            $ctrl->$method();
+
+            $cronExecutable = true ;
+
+            // Minutes
+            if ($cron->minute != "*" && date("i") != $cron->minute) {
+                $cronExecutable = false ;
+            }
+
+            // Heures
+            if ($cron->hour != "*" && date("H") != $cron->hour) {
+                $cronExecutable = false ;
+            }
+
+            // Jours
+            if ($cron->day != "*" && date("d") != $cron->day) {
+                $cronExecutable = false ;
+            }
+
+            // Mois
+            if ($cron->month != "*" && date("m") != $cron->month) {
+                $cronExecutable = false ;
+            }
+
+            // Jour de la semaine
+            if ($cron->dayOfWeek != "*" && date("w") != $cron->dayOfWeek) {
+                $cronExecutable = false ;
+            }
+
+            if ($cronExecutable) {
+                list($controller, $method) = explode("@", $cron->command);
+                $ctrl = new $controller;
+                $ctrl->$method();
+            }
         }
     }
 }
