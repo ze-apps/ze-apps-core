6bf92c1007741902e0b6b3adb94cf829a14bde1c
Aziz Bennane <aziz.bennane@preview-communication.fr>
2019-01-22 16:58:52 +0100

    Nouveau menu zeapps (contrats)

---
+++ space/contract.php
@@ -0,0 +1,10 @@
+<?php
+$spaceData = array () ;
+$spaceData["id"] = "com_zeapps_contract" ;
+$spaceData["name"] = "Contrats" ;
+$spaceData["color"] = "#ffffff" ;
+$spaceData["background-color"] = "#760692" ;
+$spaceData["menu-header"] = array() ;
+$spaceData["menu-header"]["col"] = 2 ;
+$spaceData["menu-header"]["order"] = 10 ;
+$space[] = $spaceData ;
